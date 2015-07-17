273e80a78a1eb3fd37785c0d38186caf59583bf3
good-yogurt <mai.kobari@bizreach.co.jp>
2015-07-17 19:01:15 +0900

    first commit

---
+++ sample.js
@@ -0,0 +1,8 @@
+// var a = document.getElementById('a');
+// alert(a != null);
+// var b = document.getElementById('b');
+// alert(b != null);
+// function() {
+//   var a = document.getElementById('a')
+//   var b = document.getElementById('b');
+// }
